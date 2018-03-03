91ee9b6cb28c8c3fb2e12883fe482f0084eb65b9
alichtman <aaronlichtman@gmail.com>
2018-03-03 06:30:09 -0600

    refactored and updated code

--- src/DecodeCaesar.java
+++ src/DecodeCaesar.java
@@ -1,20 +1,16 @@
-/*
- * 
- * @author aaronjl2
- */
 public class DecodeCaesar {
 
 	public static void main(String[] args) {
 
-		//Extra challenge (not graded - just for fun)
-		//Create a new program DecodeCaesar.java that tries all 25 possible shift to automatically decrypt a message.
-		//Either print all possible shifts, or only the ones where the frequency of the vowels a and e are above some threshold.
-
-		String source = "";
+		/*
+		  Tries all 25 possible shifts to produce a list of likely inputs to a caesar-cipher encoded message.
+		  Displays the decryption possibility only when the frequency of any vowel is above 5%.
+		 */
 
 		//Entering source text
-		TextIO.putln("Please enter the source text (press enter to exit)");
-		source = TextIO.getln();
+		System.out.println("Please enter caesar-cipher encoded text (press enter to exit)");
+		System.out.println("List of decryption possibilities will be printed.\n-----");
+		String source = TextIO.getln();
 		source = source.trim();
 
 		//Checking if the user wants to exit
@@ -22,26 +18,22 @@ public class DecodeCaesar {
 			TextIO.put("Bye.");
 			System.exit(0);
 		}
-		//If there is input, print the source
-		else {
-			TextIO.putln("Cyphertext  : " + source);
-		}
+
 		source = source.toUpperCase();
-		
+
 		char []sourceText = new char[source.length()];
 		int []unicode = new int[source.length()];
 		int []unicodeCopy = new int[source.length()];
 		String hex;
-		int dec = 0;
-		int count;
+		int dec, count;
 
 		//Moves source text into array
-		for(count = 0; count < source.length(); count++) {
+		for (count = 0; count < source.length(); count++) {
 			sourceText[count] = source.charAt(count);
 		}
 
 		//Go through sourceText and translate each letter into unicode value.
-		for(count = 0; count < sourceText.length; count++) {
+		for (count = 0; count < sourceText.length; count++) {
 			//Convert to hex and then to decimal representation of char
 			hex = Integer.toHexString(sourceText[count]);
 			dec = Integer.parseInt(hex, 16);
@@ -54,26 +46,26 @@ public class DecodeCaesar {
 		}
 	}
 
-	public static void smartShift(int input, int[]unicode, int[]unicodeCopy) {
+	private static void smartShift(int input, int[] unicode, int[] unicodeCopy) {
 
-		//Create a new program DecodeCaesar.java that tries all 25 possible shift to automatically decrypt a message.
-		//Either print all possible shifts, or only the ones where the frequency of the vowels a and e are above some threshold.
-
-		//Copy over values to preserve them
+		// Preserve values
 		for (int x = 0; x <= unicode.length - 1; x++) {
 			unicodeCopy[x] = unicode[x];
+
 			//Shift and recovery
 			if (unicode[x] >= 65 && unicode[x] <= 90) {
 				unicodeCopy[x] += input;
+
 				if (unicodeCopy[x] > 90) {
 					unicodeCopy[x] -= 26;
 				}
 			}
 		}
+
 		String [] processed = new String[unicode.length];
 		char [] finalProcess = new char[unicode.length];
 
-		//Converts decimal to hex string
+		//Converts dec to hex string
 		for (int count = 0; count < processed.length; count++) {
 			processed[count] = Integer.toHexString(unicodeCopy[count]);
 
@@ -91,42 +83,50 @@ public class DecodeCaesar {
 		double oTotal = 0;
 		double uTotal = 0;
 
-		//Analyzes array to count frequency of vowels
-		for(int count = 0; count < finalProcess.length; count++) {
-			if (finalProcess[count] == 'A') {
-				aTotal++;
-			}
-			if (finalProcess[count] == 'E') {
-				eTotal++;
-			}
-			if (finalProcess[count] == 'I') {
-				iTotal++;
-			}
-			if (finalProcess[count] == 'O') {
-				oTotal++;
-			}
-			if (finalProcess[count] == 'U') {
-				uTotal++;
-			}
+		//Analyze array to count frequency of vowels
+		for (char c : finalProcess) {
+
 			total++;
+
+			switch(c) {
+				case 'A':
+					aTotal++;
+					break;
+				case 'E':
+					eTotal++;
+					break;
+				case 'I':
+					iTotal++;
+					break;
+				case 'O':
+					oTotal++;
+					break;
+				case 'U':
+					uTotal++;
+					break;
+				default:
+					break;
+			}
 		}
+
 		//Frequency calculations
-		String finalCrypto = "";
-		for (int z = 0; z < finalProcess.length; z++) {
-			finalCrypto += finalProcess[z];
+		StringBuilder finalCrypto = new StringBuilder();
+
+		for (char c : finalProcess) {
+			finalCrypto.append(c);
 		}
-		if (eTotal/total >= 0.05 || aTotal/total >= 0.05 || iTotal/total >= 0.05 || oTotal/total >= 0.05 || uTotal/total >= 0.05) {
+
+		if (eTotal / total >= 0.05 || aTotal / total >= 0.05 || iTotal / total >= 0.05 || oTotal / total >= 0.05 || uTotal / total >= 0.05) {
 			TextIO.putln();
-			TextIO.putln("Potential: " + finalCrypto);
-			TextIO.putln("A Pctg: " + aTotal/total);
-			TextIO.putln("E Pctg: " + eTotal/total);
-			TextIO.putln("I Pctg: " + iTotal/total);
-			TextIO.putln("O Pctg: " + oTotal/total);
-			TextIO.putln("U Pctg: " + uTotal/total);
-		}
-		else {
-			TextIO.putln();
-			TextIO.putln("NOT Potential: " + finalCrypto);
+			System.out.println("\t" + finalCrypto);
+			System.out.println("\t\tA Pct: " + aTotal / total);
+			System.out.println("\t\tE Pct: " + eTotal / total);
+			System.out.println("\t\tI Pct: " + iTotal / total);
+			System.out.println("\t\tO Pct: " + oTotal / total);
+			System.out.println("\t\tU Pct: " + uTotal / total);
+//		} else {
+//			System.out.println();
+//			System.out.println("NOT Potential: " + finalCrypto);
 		}
 	}
 }
